1dece32514aa2069e2393eb55d1da6158c7272fe
kazim raza <kazim.raza@tintash.com>
2023-01-27 11:51:13 +0500

    what: more routes added to server file

--- server.js
+++ server.js
@@ -6,6 +6,42 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+//Create new record
+router.post('/add', (req, res) => {
+  res.send('New record added.');
+});
+
+//delete existing record
+router.delete('/', (req, res) => {
+  res.send('Deleted existing record');
+});
+
+//updating existing record
+router.put('/', (req, res) => {
+  res.send('Updating existing record');
+});
+
+//showing demo records
+router.get('/demo', (req, res) => {
+  res.json([
+    {
+      id: '001',
+      name: 'Smith',
+      email: 'smith@gmail.com',
+    },
+    {
+      id: '002',
+      name: 'Sam',
+      email: 'sam@gmail.com',
+    },
+    {
+      id: '003',
+      name: 'lily',
+      email: 'lily@gmail.com',
+    },
+  ]);
+});
+
 const HTTP_PORT = process.env.PORT || 8080;
 // setup http server to listen on HTTP_PORT
 app.listen(HTTP_PORT);
