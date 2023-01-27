6972103772a0c4f1f8d443048798fcd1b481aa65
NodeExercises <nodeexercise@gmail.com>
2023-01-27 15:47:30 +0500

    what: first commit

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const app = express();
 
-// setup a 'route' to listen on the default url path
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-//Create new record
 app.get('/add', (req, res) => {
   res.send('New record added.');
 });
 
-//showing demo records
 app.get('/demo', (req, res) => {
   res.json([
     {
@@ -33,5 +30,6 @@ app.get('/demo', (req, res) => {
 });
 
 const HTTP_PORT = process.env.PORT || 8080;
-// setup http server to listen on HTTP_PORT
-app.listen(HTTP_PORT);
+app.listen(HTTP_PORT, () => {
+  console.log(`Server is listening at port ${HTTP_PORT}`);
+});
